5c96378aad7c5348a40b3e73a2b4b0760573fff0
Abhinav <imonline.abhinav@gmail.com>
2022-07-27 22:41:47 +0530

    GitHub link on Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -14,8 +14,9 @@ const Dashboard = () => {
                 <UnitToggle />
             </div>
             {error ? <Error message={error} /> : <Widget />}
-            <div className='flex justify-between mr-8 text-sm text-shadow-sm opacity-50'>
+            <div className='flex justify-between mr-8 text-sm text-shadow-sm text-white/50'>
                 <span> Powered by <a href="https://www.weatherapi.com/" title="Weather API">WeatherAPI.com</a> </span>
+                <span className='hover:scale-105 transition duration-200'> <a className='text-white/60 text-[1rem] font-semibold no-underline' href='https://github.com/AbhiSinha08/WeatherWise.git'> GitHub </a> </span>
                 <span> Design inspired from <a href="https://dribbble.com/shots/7118235-Weather-DailyUI-037?utm_source=Clipboard_Shot&utm_campaign=thearthurk&utm_content=Weather%20%20-%20DailyUI%20037&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=thearthurk&utm_content=Weather%20%20-%20DailyUI%20037&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=thearthurk&utm_content=Weather%20%20-%20DailyUI%20037&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=thearthurk&utm_content=Weather%20%20-%20DailyUI%20037&utm_medium=Social_Share"
                     title="Weather - DailyUI - Dribble">Dribble</a> </span>
             </div>
